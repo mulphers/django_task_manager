1c22ab9da6d851777714e15155091c63cd2c3bcc
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-08-17 15:58:39 +0300

    Добавил представления для подтверждения электронного адреса

--- users/views.py
+++ users/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic.edit import CreateView, UpdateView
 
 from common.mixins import LogoutRequiredMixin, TitleMixin
 
 from .forms import ProfileUpdateForm, SignInForm, SignUpForm
 from .models import Users
+from .services import email_confirmation_check
 
 
 class SignInView(TitleMixin, LogoutRequiredMixin, LoginView):
@@ -34,3 +37,14 @@ class ProfileView(TitleMixin, LoginRequiredMixin, UpdateView):
 
     def get_success_url(self):
         return self.request.META.get('HTTP_REFERER')
+
+
+class EmailVerifiedView(View):
+    @staticmethod
+    def get(_request, *_args, **kwargs):
+        email_confirmation_check(
+            email=kwargs.get('email'),
+            code=kwargs.get('code')
+        )
+
+        return redirect('/')
