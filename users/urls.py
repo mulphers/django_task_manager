fe2fabd7b139f85af35dd567e0046db68dd047c6
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-08-17 16:00:22 +0300

    Добавил в urlpatterns маршрут для подтверждения почты

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import ProfileView, SignInView, SignUpView
+from .views import EmailVerifiedView, ProfileView, SignInView, SignUpView
 
 app_name = 'users'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('sign-in/', SignInView.as_view(), name='sign_in'),
     path('sign-up/', SignUpView.as_view(), name='sign_up'),
     path('sign-out/', LogoutView.as_view(), name='sign_out'),
-    path('profile/<int:pk>', ProfileView.as_view(), name='profile')
+    path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
+    path('email-verified/<str:email>/<uuid:code>', EmailVerifiedView.as_view(), name='verify')
 ]
