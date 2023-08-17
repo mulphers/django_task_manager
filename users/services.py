9b6e3159145d1703602baa4a78f78a3aab9f8a67
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-08-17 16:12:07 +0300

    Поместил блок кода в try

--- users/services.py
+++ users/services.py
@@ -1,10 +1,16 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import EmailVerifications, Users
 
 
 def email_confirmation_check(email, code):
-    user = Users.objects.get(email=email)
-    email_verification = EmailVerifications.objects.filter(user=user, code=code)
+    try:
+        user = Users.objects.get(email=email)
+        email_verification = EmailVerifications.objects.filter(user=user, code=code)
 
-    if email_verification.exists():
-        user.email_verified = True
-        user.save()
+        if email_verification.exists():
+            user.email_verified = True
+            user.save()
+
+    except ObjectDoesNotExist:
+        pass
