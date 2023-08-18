bbdf352f3ca264fea617330d4bfcaea7e2b66495
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-08-18 17:44:47 +0300

    Прописал маршруты для восстановления пароля

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import EmailVerifiedView, ProfileView, SignInView, SignUpView
+from .views import (ChangePasswordView, EmailVerifiedView,
+                    PasswordRecoveryView, ProfileView, SignInView, SignUpView)
 
 app_name = 'users'
 
@@ -10,5 +11,7 @@ urlpatterns = [
     path('sign-up/', SignUpView.as_view(), name='sign_up'),
     path('sign-out/', LogoutView.as_view(), name='sign_out'),
     path('profile/<int:pk>', ProfileView.as_view(), name='profile'),
-    path('email-verified/<str:email>/<uuid:code>', EmailVerifiedView.as_view(), name='verify')
+    path('email-verified/<str:email>/<uuid:code>', EmailVerifiedView.as_view(), name='verify'),
+    path('password-recovery/', PasswordRecoveryView.as_view(), name='recovery'),
+    path('change-password/<uidb64>/<token>', ChangePasswordView.as_view(), name='change')
 ]
