f221c20d37ff5fc55097b5bd29ed6fb9f8c61be9
Alexander Denisenya <alexanderdenisenya@gmail.com>
2023-08-20 15:31:52 +0300

    Добавил минимальные тесты для приложения Users

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,85 @@
-# from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse_lazy
+
+from .models import Users
+
+
+class SignUpViewTestCases(TestCase):
+    def setUp(self):
+        self.path = reverse_lazy('users:sign_up')
+        self.data = {
+            'first_name': 'Иван',
+            'last_name': 'Иванов',
+            'username': 'test_username',
+            'email': 'test_email@gmail.com',
+            'password1': '1234567pP',
+            'password2': '1234567pP'
+        }
+
+    def test_sign_up_get(self):
+        response = self.client.get(
+            path=self.path
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/sign-up.html')
+        self.assertEqual(response.context_data.get('title'), 'DT - Регистрация')
+
+    def test_sign_up_post_error(self):
+        Users.objects.create(username=self.data.get('username'))
+
+        response = self.client.post(
+            path=self.path,
+            data=self.data
+        )
+
+        self.assertContains(response, 'Пользователь с таким именем уже существует.', html=True)
+
+
+class SignInViewTestCases(TestCase):
+    def setUp(self):
+        self.path = reverse_lazy('users:sign_in')
+        self.data = {
+            'first_name': 'Иван',
+            'last_name': 'Иванов',
+            'username': 'test_username',
+            'email': 'test_email@gmail.com',
+            'password': '1234567pP',
+        }
+
+    def test_sign_in_get(self):
+        response = self.client.get(
+            path=self.path
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, 'users/sign-in.html')
+        self.assertEqual(response.context_data.get('title'), 'DT - Авторизация')
+
+    def test_sign_in_post_error(self):
+        Users.objects.create(**self.data)
+
+        response = self.client.post(
+            path=self.path,
+            data={
+                'username': self.data.get('username'),
+                'password': '...'
+            }
+        )
+
+        self.assertContains(response, 'Пожалуйста, введите правильные имя пользователя и пароль.'
+                                      ' Оба поля могут быть чувствительны к регистру.', html=True)
+
+
+class ProfileViewTestCases(TestCase):
+    def setUp(self):
+        self.path = reverse_lazy('users:profile', kwargs={'pk': 1})
+
+    def test_profile_get_without_sign_in(self):
+        response = self.client.get(
+            path=self.path
+        )
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
